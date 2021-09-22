dc2a8e428f29f4f217a7e845802998c75dc96768
ptaushanov <pepi15032000@gmail.com>
2021-09-22 19:51:24 +0300

    feat: hidding inactive cards

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,9 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  document
+  .querySelectorAll(".card:not(.active)")
+  .forEach(card => {
+    setTimeout(()=>{card.style.display = "none"}, 3000);
+  })
 });
